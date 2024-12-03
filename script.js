c146af47b8813ffcf2c03b370b8ee129be63d42b
Jus-netizen <drtj168@gmail.com>
2024-12-02 19:17:28 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const detailsContent = [
+  {
+    title: "Day 1: Lower Body",
+    exercises: [
+      "Goblet Squats - 3 sets x 12-15 reps",
+      "Hip Thrusts - 3 sets x 12-15 reps",
+      "Romanian Deadlifts - 3 sets x 10-12 reps",
+     
